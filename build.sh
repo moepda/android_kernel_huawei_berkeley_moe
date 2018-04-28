aeb9f5281a1cf82310bb58493d1ede2c242c87e4
acuicultor <ramoncuenca4@gmail.com>
2018-04-28 21:44:54 +0200

    Build.sh: add build host

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH=$PATH:/home/moepda/duo/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+export CROSS_COMPILE=aarch64-linux-android-
+export KBUILD_BUILD_HOST=unicornblood
+mkdir out
+make ARCH=arm64 O=out unicornblood_defconfig
+make ARCH=arm64 O=out -j5
+
