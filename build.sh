f93e5714ff0272de489142b7b82fb06e3197d5d8
moepda <moepda@gmail.com>
2018-06-01 16:01:29 +0100

    Unicornblood defconfig - berkeley
    Signed-off-by: moepda <moepda@gmail.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export PATH=$PATH:/home/moepda/duo/prebuilts/gcc/linux-x86/aarch64/aarch64-linux
 export CROSS_COMPILE=aarch64-linux-android-
 export KBUILD_BUILD_HOST=unicornblood
 mkdir out
-make ARCH=arm64 O=out unicornblood_defconfig
+make ARCH=arm64 O=out merge_kirin970_defconfig
 make ARCH=arm64 O=out -j5
 
